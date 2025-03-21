aa59eb31eaaadaeb7d882b2de112a96c164c26f4
DreamerHDR <morozov95080@gmail.com>
2025-03-21 15:50:45 +0300

    qwe test mess

---
+++ qwe.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "132";
+
+$qwe;
+$qwe = mysqli_query($conn,"");
+
+
+class Qwe{
+	
+	public $products = array();
+	public $storage = array();
+
+	public function __construct(){
+		$this->products = array();
+		$this->storage = array();
+	}
+}
